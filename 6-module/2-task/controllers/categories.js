af182c77ae339169ec3a38af3c01a92a39e38e6a
gedrimas <a.a.gedrimas@gmail.com>
2019-10-16 23:37:33 +0300

    6-module 2-task not end

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -2,8 +2,19 @@ const Category = require('../models/Category')
 module.exports.categoryList = async function categoryList(ctx, next) {
   const categories = await Category.find();
   if(categories.length > 0) {
-    ctx.body = {categories}
-    //console.log('BODY', ctx.body)
+    const categoriesArr = categories.map(itm => {
+      return {
+        id: itm.id,
+        title: itm.title,
+        subcategories: itm.subcategories.map(itm => {
+          return {
+            id: itm.id,
+            title: itm.title,
+          }
+        })
+      }
+    })
+    ctx.body = {categories: categoriesArr}
   }else{
     ctx.body = {categories: []};
   }
--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,10 +1,15 @@
 const Product = require('../models/Product')
 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  const productSubcategory = await Product.find().populate('subcategory')
-  console.log('SUB', productSubcategory)
+  const productSubcategory = await Product.findOne().populate('subcategory')
+console.log('productSubcategory', productSubcategory)
   if(productSubcategory.length > 0){
-    ctx.subId = productSubcategory.subcategory 
+    const subIds = productSubcategory.map(itm => {
+      return itm.subcategory
+    })
+
+    console.log('subIds', subIds)
+    //ctx.subId = productSubcategory.subcategory 
   }else{
     ctx.body = {products: []};
   }
@@ -14,6 +19,7 @@ module.exports.productList = async function productList(ctx, next) {
   const products = await Product.find({subcategory: ctx.subId})
   if(products.length > 0){
     ctx.body = {products}
+    console.log('products', products)
   }else{
     ctx.body = {products: []};
   }
