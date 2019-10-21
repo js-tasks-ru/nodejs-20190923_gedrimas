37f138e7f1775689e929d677cf4afbe5a12381c0
gedrimas <a.a.gedrimas@gmail.com>
2019-10-21 20:04:44 +0300

    products bug

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,31 +1,40 @@
 const Product = require('../models/Product')
 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  const productSubcategory = await Product.findOne().populate('subcategory')
-console.log('productSubcategory', productSubcategory)
-  if(productSubcategory.length > 0){
-    const subIds = productSubcategory.map(itm => {
-      return itm.subcategory
-    })
-
-    console.log('subIds', subIds)
-    //ctx.subId = productSubcategory.subcategory 
-  }else{
-    ctx.body = {products: []};
-  }
-};
+  const {subcategory} = ctx.request.query; 
+  ctx.subcategory = subcategory
+  next()
+}  
 
 module.exports.productList = async function productList(ctx, next) {
-  const products = await Product.find({subcategory: ctx.subId})
+  const products = await Product.find({subcategory: ctx.subcategory})
+
   if(products.length > 0){
-    ctx.body = {products}
-    console.log('products', products)
+     const productsArr = products.map(itm => {
+       return {
+         id: itm.id,
+         title: itm.title,
+         images: itm.images,
+         category: itm.category,
+         subcategory: itm.subcategory,
+         price: itm.price,
+         description: itm.description,
+       }
+     })
+    ctx.status = 200; 
+    ctx.body = {products: productsArr};
   }else{
+    console.log('1111111111111111');
+    ctx.status = 200;
     ctx.body = {products: []};
   }
 };
 
 module.exports.productById = async function productById(ctx, next) {
+  console.log('3333333333333333')
+  
+  ctx.status = 200;
   ctx.body = {product: {}};
+  
 };
 
